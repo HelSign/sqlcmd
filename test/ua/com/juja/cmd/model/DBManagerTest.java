8943dd361aa0c3046493ac06cfabadd547852770
Helena <hneshcheret@gmail.com>
2018-04-04 15:28:26 +0300

    Integration tests were added

---
+++ test/ua/com/juja/cmd/model/DBManagerTest.java
@@ -0,0 +1,23 @@
+package ua.com.juja.cmd.model;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public abstract class DBManagerTest {
+    private DBManager dbManager;
+
+    @Before
+    public void setup(){
+        dbManager = getDBManager();
+        dbManager.makeConnection("sqlcmd","postgres","postgres");
+    }
+
+    abstract DBManager getDBManager();
+
+    @Test
+    public void testIsConnected(){
+        assertTrue(dbManager.isConnected());
+    }
+}
