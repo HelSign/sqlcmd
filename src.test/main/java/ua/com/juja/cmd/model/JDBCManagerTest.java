427539a4288aa62bcd151ff846bfe27b62c5f06e
Helena <hneshcheret@gmail.com>
2018-04-16 23:17:51 +0300

    trying to work with git#2

--- src.test/main/java/ua/com/juja/cmd/model/JDBCManagerTest.java
+++
@@ -1,8 +0,0 @@
-package java.ua.com.juja.cmd.model;
-
-public class JDBCManagerTest extends DBManagerTest {
-    @Override
-    DBManager getDBManager() {
-        return new JDBCManager();
-    }
-}
