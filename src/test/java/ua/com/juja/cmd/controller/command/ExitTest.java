30ba9ade4be9d033a2197632611b71ba3f1c65ef
Helena <hneshcheret@gmail.com>
2018-05-04 12:52:23 +0300

    adding unit test for Command Exit

--- src/test/java/ua/com/juja/cmd/controller/command/ExitTest.java
+++ src/test/java/ua/com/juja/cmd/controller/command/ExitTest.java
@@ -1,4 +1,35 @@
 package ua.com.juja.cmd.controller.command;
 
+import org.junit.Before;
+import org.junit.Test;
+import ua.com.juja.cmd.view.Console;
+import ua.com.juja.cmd.view.View;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
 public class ExitTest {
+    private View view;
+    private Command command;
+
+    @Before
+    public void setup() {
+        view = new Console();
+        command = new Exit(view);
+    }
+
+    @Test(expected = ExitException.class)
+    public void testExecute() {
+        command.execute("exit");
+    }
+
+    @Test
+    public void testIsExecutable() {
+        assertTrue(command.isExecutable("exit"));
+    }
+
+    @Test
+    public void testIsNotExecutable() {
+        assertFalse(command.isExecutable("1exit"));
+    }
 }
